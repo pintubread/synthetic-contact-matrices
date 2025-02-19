62807616b8120de30e0eec6f093d8765562421f8
pintubread <pintubreadboyle@gmail.com>
2025-02-19 22:35:36 +0800

    pull data

---
+++ generate_synthetic_matrices/pull_data.R
@@ -0,0 +1,18 @@
+#load('input/pop/poptotal.rdata')
+load('input/pop/popratio.rdata')
+#load('input/pop/popfemale.rdata')
+load('input/pop/popratio_female.rdata')
+load('input/pop/popratio_male.rdata')
+load('input/pop/urbanicity.rdata')
+load('input/pop/popUrban.rdata')
+load('input/pop/popRural.rdata')
+load('input/wbindicators.rdata') #cannot work, pull from country_indicators
+
+load('input/work/workpopage.rdata') #convert to percentage
+load('input/school/enrolment.rdata')
+load('input/school/end_age_school.rdata')
+load('input/school/start_age_school.rdata')
+load('input/school/pupiltoteacherratio.rdata')
+load('input/school/secteachers.rdata') # convert to percentage
+load('input/school/students.rdata') #convert to percentage
+load('input/school/teachers.rdata')
